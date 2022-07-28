da87a1e2f9190b16b1997fc69d22b1bffb50d55d
Stefan Frede <stefan@frede.dev>
2022-07-28 16:36:32 +0200

    feat: first commit

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 50],
+    'body-max-line-length': [2, 'always', 72],
+    'footer-max-line-length': [2, 'always', 72],
+  },
+};
